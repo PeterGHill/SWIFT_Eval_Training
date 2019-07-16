7f2ddf136a73c030eff945428c20a5aa5ed936ea
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-16 16:33:09 +0100

    Fix a few errors

--- read_gpm.py
+++ read_gpm.py
@@ -4,6 +4,8 @@ Read GPM calibrated precipitation for given area and time period
 
 from netCDF4 import Dataset
 import numpy as np
+import datetime
+import glob
 
 gpm_dir = '/gws/nopw/j04/swift/GPM/' # Directory on Jasmin, will need to change.
 gpm_res = 0.1
@@ -22,8 +24,8 @@ def read_gpm(start_time, lon_min, lon_max, lat_min, lat_max, end_time=-9999, var
         ncfile = Dataset(f)
         lons = ncfile.variables['lon'][:]
         lats = ncfile.variables['lat'][:]
-        ind_lon = np.where((lon >= lon_min) & (lon <= lon_max))[0]
-        ind_lat = np.where((lat >= lat_min) & (lat <= lat_max))[0]
+        ind_lon = np.where((lons >= lon_min) & (lons <= lon_max))[0]
+        ind_lat = np.where((lats >= lat_min) & (lats <= lat_max))[0]
         if ncfile.variables[varname].ndim == 3: # Some GPM files have an extra dimension - don't understand why
             rain[i,:,:] = ncfile.variables[varname][0,ind_lon[0]:ind_lon[-1]+1, ind_lat[0]:ind_lat[-1]+1]
         elif ncfile.variables[varname].ndim == 2:
@@ -40,9 +42,12 @@ def get_gpm_filelist(start_time, end_time):
     Note this depends on the directory structure in place.
     '''
     n_times = (end_time - start_time).total_seconds() / (60 * 30.) + 1
-    timelist = [start_time + datetime.timedelta(seconds=60 * 30. * n) for n in n_times]
+    if n_times > 1:
+        timelist = [start_time + datetime.timedelta(seconds=60 * 30. * n) for n in n_times]
+    else:
+        timelist = [start_time]
     filelist = []
     for time in timelist:
-        f += [glob.glob(gpm_dir + time.strftime('%Y/%m/%d/*%Y%m%d-S%H%M*.nc'))] # Note assumes only one file for each timestamp
+        filelist += glob.glob(gpm_dir + time.strftime('%Y/%m/%d/*%Y%m%d-S%H%M*.nc')) # Note assumes only one file for each timestamp
     return timelist, filelist
 
