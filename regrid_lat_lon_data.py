2ef58d14a94f104430b3fa54a2f95e62281ef609
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-22 16:00:33 +0100

    Create regrid_lat_lon_data.py

---
+++ regrid_lat_lon_data.py
@@ -0,0 +1,26 @@
+from scipy.interpolate import griddata
+
+
+def regrid(lon, lat, data, method='cubic', new_lat=np.arange(0.5,30), new_lon=np.arange(-19.5,20), min_val=0.0):
+#   Note, this assumes that data.shape = (Lat, lon) or (:, lat, lon).
+    data_shape = data.shape
+    n_lon = new_lon.size
+    n_lat = new_lat.size
+    if ((lon.ndim == 1) and (lat.ndim ==1)):
+        lat, lon = np.meshgrid(lat, lon)
+    points = np.dstack((lat.flatten(), lon.flatten()))[0,:,:]
+    x1,x2 = np.meshgrid(new_lat, new_lon)
+    new_points = np.dstack((x1.flatten(), x2.flatten()))[0,:,:]
+    if data.ndim == 2:
+        values = data.flatten()
+        ind = np.where(values >= min_val)
+        values = values[ind]
+        points = points[ind]
+        result = griddata(points, values, new_points, method=method, fill_value=-np.NaN).reshape(n_lon, n_lat) # works for gerb
+    else:
+        result = []
+        for i in range(data_shape[0]):
+            values = data[i,:,:].flatten()
+            result += [griddata(points, values, new_points, method=method, fill_value=-np.NaN).reshape(n_lat, n_lon)]
+        result = np.array(result) # works for era-interim
+    return result
