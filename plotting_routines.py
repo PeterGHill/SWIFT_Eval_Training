633afde2ddf6bfda9789b84ecf6ba34f371075cd
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-28 13:59:18 +0100

    fix longitudes on map and fig create

--- plotting_routines.py
+++ plotting_routines.py
@@ -1,4 +1,11 @@
-def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=plt.figure(), subplot='111', savename='', cbar_levels=np.arange(10), cbar_label='mm/day'):
+import matplotlib.pyplot as plt
+import numpy as np
+import cartopy
+import matplotlib
+matplotlib.rcParams.update({'font.size': 18})
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=-9999, subplot='111', savename='', cbar_levels=np.arange(10), cbar_label='mm/day'):
     """
     This function plots a map showing the data
     data should be a 2D array with dimensions lat|lon
@@ -8,10 +15,12 @@ def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=plt.figure(), subplot='1
     """
 
     # Generate axes
+    if fig == -9999:
+        fig = plt.figure()
     ax = fig.add_subplot(subplot, projection=cartopy.crs.PlateCarree())
 
     # Contours and colour bar
-    cp=plt.contourf(lon, lat,mean_rainfall ,transform=cartopy.crs.PlateCarree(), cmap = 'Spectral', levels =cbar_levels,extend='max' )
+    cp=plt.contourf(lon, lat,data ,transform=cartopy.crs.PlateCarree(), cmap = 'Spectral', levels =cbar_levels,extend='max' )
     cbar = plt.colorbar(cp)
     cbar.set_label(cbar_label)
 
@@ -21,7 +30,7 @@ def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=plt.figure(), subplot='1
     #ax.add_feature(cartopy.feature.OCEAN, facecolor='white')
 
     # Grid lines
-    gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True,linewidth=1, color='black', alpha=0.5, linestyle='--', xlocs=np.arange(int(np.min(lon)),int(np.max(lon))+3,2))
+    gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True,linewidth=1, color='black', alpha=0.5, linestyle='--')
     gl.xlabels_top = False
     gl.ylabels_right = False
     gl.xformatter = LONGITUDE_FORMATTER
@@ -37,8 +46,10 @@ def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=plt.figure(), subplot='1
         plt.show()
 
         
-def scatter_plot(data1, data2, title1, title2, fig=plt.figure(), subplot='111', savename=''):
+def scatter_plot(data1, data2, title1, title2, fig=-9999, subplot='111', savename=''):
     # Generate axes
+    if fig == -9999:
+        fig = plt.figure()
     ax = fig.add_subplot(subplot)
     plt.plot(data1, data2, 'ko')
     plt.xlabel(title1)
@@ -53,7 +64,9 @@ def scatter_plot(data1, data2, title1, title2, fig=plt.figure(), subplot='111', 
         plt.show()
 
         
-def timeseries_plot(data, time, varname, fig=plt.figure(), subplot='111', savename=''):
+def timeseries_plot(data, time, varname, fig=-9999, subplot='111', savename=''):
+    if fig == -9999:
+        fig = plt.figure()
     ax = fig.add_subplot(subplot)
     plt.plot(time, data)
     plt.xlabel('Time')
