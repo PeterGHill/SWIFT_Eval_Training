f2942f68a5d459e7505567401f4b87709aa38db4
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-28 17:10:19 +0100

    Add averag_regrid which downscales

--- regrid_lat_lon_data.py
+++ regrid_lat_lon_data.py
@@ -25,3 +25,23 @@ def regrid(lat, lon, data, method='cubic', new_lat=np.arange(0.5,30), new_lon=np
             result += [griddata(points, values, new_points, method=method, fill_value=-np.NaN).reshape(n_lat, n_lon)]
         result = np.array(result) # works for era-interim
     return result
+
+
+def average_regrid(lon, lat, data, new_lat=np.arange(0.5,30), new_lon=np.arange(-19.5,20), min_val=0.0, times_n = 1):
+    data[data < min_val] = np.nan
+    lat_bin_size = new_lat[1]-new_lat[0]
+    lon_bin_size = new_lon[1]-new_lon[0]
+    if times_n > 1:
+        result = np.zeros((times_n, new_lon.size, new_lat.size))
+    else:
+        result = np.zeros((new_lon.size, new_lat.size))
+    for i in range(new_lat.size):
+        for j in range(new_lon.size):
+            ind = np.where((abs(lon - new_lon[j]) <= lon_bin_size/2.0) &
+                           (abs(lat - new_lat[i]) <= lat_bin_size/2.0))
+            if times_n > 1:
+                for k in range(times_n):
+                    result[k, j, i] = np.nanmean(data[k,:,:][ind])
+            else:
+                result[j, i] = np.nanmean(data[ind])
+    return result
