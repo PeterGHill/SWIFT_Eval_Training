424882653b8fc8605878d0cb142d1ebde7ff6946
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-29 00:32:46 +0100

    Update plotting_routines.py

--- plotting_routines.py
+++ plotting_routines.py
@@ -4,6 +4,8 @@ import cartopy
 import matplotlib
 matplotlib.rcParams.update({'font.size': 18})
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import matplotlib.dates as mdates
+
 
 def plot_map(data,lat,lon, title='TAMSAT Rainfall', fig=-9999, subplot='111', savename='', cbar_levels=np.arange(10), cbar_label='mm/day'):
     """
@@ -52,12 +54,18 @@ def scatter_plot(data1, data2, title1, title2, fig=-9999, subplot='111', savenam
         fig = plt.figure()
     ax = fig.add_subplot(subplot)
     plt.plot(data1, data2, 'ko')
+    plt.plot(data1, data1, 'b')
+    plt.plot(data2, data2, 'b')
     plt.xlabel(title1)
     plt.ylabel(title2)
-    mean_diff = data1.mean() - data2.mean()
-    correlation = np.corrcoef(data1, data2)
-    rms_diff = np.sqrt(np.mean((data1-data2)**2))
-    plt.text(0.25, 0.75, 'Difference='+mean_diff+'\nR2='+correlation+'\nRMSD='+rms_diff, horizontalalignment='left', verticalalignment='top',transform=ax.transAxes)
+    mean_diff = f'Difference={(np.nanmean(data1) - np.nanmean(data2)):.2f}'
+    data1 = np.ma.masked_array(data1, mask = np.isnan(data1))
+    data2 = np.ma.masked_array(data2, mask = np.isnan(data2))
+    correlation = f'R2={np.ma.corrcoef(data1, data2)[0,1]:.2f}'
+    rms_diff = f'RMSD={np.sqrt(np.mean((data1-data2)**2)):.2f}'
+    plt.text(0.025, 0.975, mean_diff, horizontalalignment='left', verticalalignment='top',transform=ax.transAxes)
+    plt.text(0.025,0.85, correlation, horizontalalignment='left', verticalalignment='top',transform=ax.transAxes)
+    plt.text(0.025, 0.9125, rms_diff, horizontalalignment='left', verticalalignment='top',transform=ax.transAxes)
     if int(subplot[2]) == int(subplot[0])*int(subplot[1]):
         if savename != '':
             plt.savefig(savename)
@@ -71,6 +79,8 @@ def timeseries_plot(data, time, varname, fig=-9999, subplot='111', savename=''):
     plt.plot(time, data)
     plt.xlabel('Time')
     plt.ylabel(varname)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    fig.autofmt_xdate()
     if int(subplot[2]) == int(subplot[0])*int(subplot[1]):
         if savename != '':
             plt.savefig(savename)
