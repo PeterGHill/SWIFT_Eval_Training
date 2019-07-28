7515cd2eb44dd85225a658053068240968d52a04
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-28 09:53:12 +0100

    Rough outline

---
+++ practical_one.py
@@ -0,0 +1,31 @@
+'''
+Code to run for the first practical exercise in the SWIFT evaluation workshop
+Demonstrates how to load, manupulate and plot TAMSAT and GPM data.
+Focuses on 26-30 June 2018
+'''
+
+import numpy as np
+
+# Load TAMSAT data
+
+
+# Plot TAMSAT map
+
+
+# Load GPM data
+
+
+# Plot GPM map
+
+
+# regrid both datasets to same resolution
+
+
+# Plot TAMSAT, GPM and difference maps at new resolution
+
+
+# Scatterplot comparison, incuding correlations
+
+
+
+# 
