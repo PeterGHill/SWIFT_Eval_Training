db7a00c0cee6eca8eed636d1c03c4b43b08c3fd4
PeterGHill <p.g.hill@reading.ac.uk>
2019-07-28 12:29:13 +0100

    Add code to regrid and produce map plots

--- practical_one.py
+++ practical_one.py
@@ -8,6 +8,7 @@ import numpy as np
 import open_plot_TAMSAT_netCDF
 import plotting_routines
 import read_gpm
+import regrid_lat_lon_data
 
 # Load TAMSAT daily data. Load different days by editing the filename.
 filename = r"C:\Users\PeterHill\SWIFT_Eval_Practical\TAMSAT\rfe2018_06_28.v3.nc" # Need to change to match the location of the file on your computer. 
@@ -17,23 +18,33 @@ t_rain, t_lat, t_lon, t_datevar = open_plot_TAMSAT_netCDF.open_TAMSAT_netCDF(fil
 plotting_routines.plot_map(t_rain.mean(axis=0), t_lat, t_lon, savename='TAMSAT_rainfall_map_28June2018.png') # Change savename as approrpitate
 
 # Load GPM data
-start_time=datetime.datetime()
-end_time=datetime.datetime()
-lon_min= # West boundary of domain
-lon_max=
-lat_min=
-lat_max
+start_time = datetime.datetime(2018,6,28,0,0,0)
+end_time = datetime.datetime(2018,6,28,23,30,0,0)
+lon_min = -25. # West boundary of domain
+lon_max = 55. # East boundary
+lat_min = -15. # South boundary
+lat_max = 25. # North boundary
 g_rain, g_lat, g_lon, g_datevar = read_gpm.read_gpm(start_time, lon_min, lon_max, lat_min, lat_max, end_time=end_time)
+print "g_rain.shape=", g_rain.shape
 
 # Plot GPM map
 plotting_routines.plot_map(g_rain.mean(axis=0), g_lat, g_lon, savename='GPM_IMERG_rainfall_map_28June2018.png') # Change savename as approrpitate
 
+# Plot GPM diurnal cycle
+
 
 # regrid both datasets to same resolution
-
+new_res = 0.25 # new resolution in degrees change to look at how differences change with averaging scales
+new_lat = np.arange(lat_min, lat_max, new_res)
+new_lon = np.arange(lon_min, lon_max, new_res)
+regridded_g_rain = regrid_lat_lon_data.regrid(g_lat, g_lon, g_rain, method='cubic', new_lat=new_lat, new_lon=new_lon, min_val=0.0)
+regridded_t_rain = regrid_lat_lon_data.regrid(t_lat, t_lon, t_rain, method='cubic', new_lat=new_lat, new_lon=new_lon, min_val=0.0)
 
 # Plot TAMSAT, GPM and difference maps at new resolution
-
+plotting_routines.plot_map(regridded_t_rain.mean(axis=0), new_lat, new_lon, savename='Regridded_TAMSAT_rainfall_map_28June2018.png') # Change savename as approrpitate
+plotting_routines.plot_map(regridded_g_rain.mean(axis=0), new_lat, new_lon, savename='Regridded_GPM_IMERG_rainfall_map_28June2018.png') # Change savename as approrpitate
+difference = regridded_t_rain.mean(axis=0) - regridded_g_rain.mean(axis=0)
+plotting_routines.plot_map(difference, new_lat, new_lon, savename='TAMSAT_minus_GPM_rainfall_map_28June2018.png') # Change savename as approrpitate
 
 # Scatterplot comparison, incuding correlations
 
