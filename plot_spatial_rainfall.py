7fd59ef5124220da8625c0e523abb5e16e432e90
carlocafaro <carlo.cafaro89@gmail.com>
2019-07-30 09:32:12 +0100

    Update plot_spatial_rainfall.py

--- plot_spatial_rainfall.py
+++ plot_spatial_rainfall.py
@@ -1,11 +1,18 @@
 import reading_gpm_data as gpm
 import agreement as agr
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap, shiftgrid,interp
+#from mpl_toolkits.basemap import Basemap, shiftgrid,interp
 import netCDF4 as nc
 import numpy as np
 import reading_model_data as mod
 import plot_agreement as plot
+import cartopy.crs as ccrs
+import cartopy
+import matplotlib
+matplotlib.rcParams.update({'font.size': 18})
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import matplotlib.dates as mdates
+
 
 def plot_rainfall_acc_scale_obs(date,start_time,end_time,latmin,latmax,lonmin,lonmax):
 
@@ -29,20 +36,41 @@ def plot_rainfall_acc_scale_obs(date,start_time,end_time,latmin,latmax,lonmin,lo
     loncpb=indexlonobs[len(indexlonobs)-1]+1
     
     
-    x,y,m=plot.mapProjection(lonobs,latobs,latmin,latmax,lonmin,lonmax,res='i')
     
+
+
     
-   
+    title='GPM-IMERG accumulation on '+str(date)
+    levels=np.linspace(0.1,50,10)
     obs=gpm.obs_precip_accumulation_over_a_time_period(date,start_time,end_time,latmin,latmax,lonmin,lonmax)
+
+    fig=plt.figure()
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    # Contours and colour bar
+    cp=plt.contourf(lonobs, latobs,obs ,transform=ccrs.PlateCarree(), cmap = 'YlGnBu', levels =levels,extend='max' )
+    cbar = plt.colorbar(cp)
+    cbar_label=np.linspace(0,50,10)
+    cbar.set_label(cbar_label)
+
+    # Map features
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS)
+    #ax.add_feature(cartopy.feature.OCEAN, facecolor='white')
+
+    # Grid lines
+    gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True,linewidth=1, color='black', alpha=0.5, linestyle='--')
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+    # Extent and title
+    ax.set_extent((lonmin,lonmax,latmin,latmax), crs=cartopy.crs.PlateCarree())
+    plt.title(title)
     
     
-    N_bands=7
-    levels= np.linspace(0, 100, num=N_bands+1) 
-    plt.contourf(x,y,obs,levels,cmap='YlGnBu',vmin=0,vmax=100,extend='max')
-    #m.plot(xN,yN,'bo',markersize=8)
-    plt.colorbar(orientation='horizontal')
-        
-    plt.show() 
+    plt.show()
     return
 
 
@@ -66,26 +94,46 @@ def plot_spatial_rainfall_acc_cp_model(date,valid_date,init_time,start_time,end_
     latcpb=indexlatobs[len(indexlatobs)-1]+1
     loncpa=indexlonobs[0]
     loncpb=indexlonobs[len(indexlonobs)-1]+1
-    
-    
-    x,y,m=plot.mapProjection(lonobs,latobs,latmin,latmax,lonmin,lonmax,res='i')
+
+    cp_precip=mod.cp_model_accumulation_regridded(date,init_time,start_time,end_time)[latcpa:latcpb,loncpa:loncpb]
+   
     
     
    
-    cp_precip=mod.cp_model_accumulation_regridded(date,init_time,start_time,end_time)[latcpa:latcpb,loncpa:loncpb]
     
+    title='CP deterministic accumulation_init_date'+str(date)+'times_T+'+str(start_time)+'-'+str(end_time)
     
-    N_bands=7
-    levels= np.linspace(0, 100, num=N_bands+1) 
-    plt.contourf(x,y,cp_precip,levels,cmap='YlGnBu',vmin=0,vmax=100,extend='max')
-    #m.plot(xN,yN,'bo',markersize=8)
-    plt.colorbar(orientation='horizontal')
-        
-    plt.show() 
+    fig=plt.figure()
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    # Contours and colour bar
+    levels=np.linspace(0.1,50,10)
+    cp=plt.contourf(lonobs, latobs,cp_precip,transform=ccrs.PlateCarree(), cmap = 'YlGnBu', levels =levels,extend='max' )
+    cbar = plt.colorbar(cp)
+    cbar_label=np.linspace(0,50,10)
+    cbar.set_label(cbar_label)
+
+    # Map features
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS)
+    #ax.add_feature(cartopy.feature.OCEAN, facecolor='white')
+    
+    # Grid lines
+    gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True,linewidth=1, color='black', alpha=0.5, linestyle='--')
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+    # Extent and title
+    ax.set_extent((lonmin,lonmax,latmin,latmax), crs=cartopy.crs.PlateCarree())
+    plt.title(title)
+
+    plt.show()
     return
 
 
-def plot_spatial_rainfall_acc_global_model(date,valid_date,init_time,start_time,end_time,latmin,latmax,lonmin,lonmax):
+def plot_rainfall_acc_global_model(date,valid_date,init_time,start_time,end_time,latmin,latmax,lonmin,lonmax):
 
     filenameobs='C:/Users/carlo/Documents/Summer_School_evaluation/Data/gpm_imerg_production_V06B_'+str(valid_date)+'.nc'
     d=nc.Dataset(filenameobs)
@@ -114,13 +162,31 @@ def plot_spatial_rainfall_acc_global_model(date,valid_date,init_time,start_time,
     glob_precip=mod.global_model_accumulation_regridded(date,init_time,start_time,end_time)[latcpa:latcpb,loncpa:loncpb]
     
     
-    N_bands=7
-    levels= np.linspace(0, 100, num=N_bands+1) 
-    plt.contourf(x,y,glob_precip,levels,cmap='YlGnBu',vmin=0,vmax=100,extend='max')
-    #m.plot(xN,yN,'bo',markersize=8)
-    plt.colorbar(orientation='horizontal')
-        
-    plt.show() 
+    fig=plt.figure()
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    # Contours and colour bar
+    levels=np.linspace(0.1,50,10)
+    cp=plt.contourf(lonobs, latobs,glob_precip,transform=ccrs.PlateCarree(), cmap = 'YlGnBu', levels =levels,extend='max' )
+    cbar = plt.colorbar(cp)
+    cbar_label=np.linspace(0,50,10)
+    cbar.set_label(cbar_label)
+
+    # Map features
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS)
+    #ax.add_feature(cartopy.feature.OCEAN, facecolor='white')
+
+    # Grid lines
+    gl = ax.gridlines(crs=cartopy.crs.PlateCarree(), draw_labels=True,linewidth=1, color='black', alpha=0.5, linestyle='--')
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+    # Extent and title
+    ax.set_extent((lonmin,lonmax,latmin,latmax), crs=cartopy.crs.PlateCarree())
+    plt.title(title) 
     return
 
 
